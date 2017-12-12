92eb30258a598b3ece4a67a319dbca531877a118
paulojorge <pauloj.fac@gmail.com>
2017-12-12 21:54:02 -0200

    arrumando conflito de merge

--- App/app.py
+++ App/app.py
@@ -3,11 +3,7 @@
 *
 *
 """
-<<<<<<< HEAD
-from flask import Flask, jsonify,url_for
-=======
 from flask import Flask, jsonify, url_for, render_template
->>>>>>> a3e95c3926703073c36dbbcd80bf8e07cde73dd2
 import random
 import os
 
@@ -22,27 +18,6 @@ imagens = motor(file)
 
 @app.route("/v1",methods=['GET'])
 def json_api():
-<<<<<<< HEAD
-	
-	imagem = '<img src="albatroz-app/App/640full-amanda-lee-(i).jpg" />'
-	filename = os.listdir('../static/img')
-	print(imagem)
-
-	#noticias = [{ "imagem" : img }]			
-	
-	return u'<img src='+filename[0]+'/>'
-	'''
-	<html>
-		<head>
-			<title>Spotipo</title>
-		</head>
-		<body>
-			<img src=%s />
-		</body>
-	</html>
-	''' 
-	#return jsonify(noticias=noticias)
-=======
 	img = '/static/img/640full-amanda-lee-(i).jpg'
 	img2 = 'http://localhost:5000'+img
 	imagem =[{'img': img2}]
@@ -50,7 +25,6 @@ def json_api():
 	#return '<img src='+img+' />'
 	return jsonify(imagem=imagem)
 
->>>>>>> a3e95c3926703073c36dbbcd80bf8e07cde73dd2
 
 @app.route("/img/m")
 def json_img():
