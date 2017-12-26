2267d63d577eec1fee67b850c11643bb60a99119
paulojorge <paulojorge@nox5.com>
2017-12-26 14:38:17 -0200

    Added 'User' class

--- app/models.py
+++ app/models.py
@@ -29,3 +29,19 @@ class Image(db.Model):
 
 	def __repr__(self):
 		return '{}'.format(self.img_url)
+
+class User(db.Model):
+	__tablename__ = "users"
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String, nullable=True)
+	email = db.Column(db.String, nullable=True)
+	username = db.Column(db.String, nullable=True)
+
+	def __init__(self, name, email, username):
+		self.name = name
+		self.email = email
+		self.username = username
+
+	def __repr__(self):
+		return 'name: {}, email: {}, username: {}'.format(self.name, self.email, self.username)
